b326575bd1462ba7190ed847f05a20a0a10b558f
Noman Shamim <nomanshamim2004@gmail.com>
2024-06-30 19:32:52 +0500

    Working on routing....

---
+++ front-end/src/Components/Navigations/Navigation.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '../Pages/Home/Home';
+import Signup from '../Pages/Signup/Signup';
+import Login from '../Pages/Login/Login';
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/signup",
+        element: <Signup />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+])
+
+
+export default function Navigation() {
+    return (
+        <RouterProvider router={router} />
+    )
+}

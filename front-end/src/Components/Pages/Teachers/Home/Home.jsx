62b25f78ef7db3339480e76bdb7f78e539e172d1
Noman Shamim <nomanshamim2004@gmail.com>
2024-07-05 21:40:04 +0500

    Header , Footer Component done.

--- front-end/src/Components/Pages/Teachers/Home/Home.jsx
+++ front-end/src/Components/Pages/Teachers/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbars from '../../../Navbars/Navbars'
+import Footer from '../../../Footer/Footer'
 
 export default function Home() {
     return (
         <div>
             <Navbars />
+            <Footer />
         </div>
     )
 }

b4abcc0869a20aa9da38db305136e7c3eb701f19
Noman Shamim <nomanshamim2004@gmail.com>
2024-07-15 00:22:13 +0500

    Notification-Modal done.

--- front-end/src/Components/Pages/Home/Home.jsx
+++ front-end/src/Components/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbars from '../../Navbars/Navbars'
 import Footer from '../../Footer/Footer'
 import StudentHomePage from '../Students/StudentHomePage'
+import NotificationModal from '../../NotificationModal/NotificationModal'
 
 export default function Home() {
   return (
     <div>
       <Navbars />
+      {/* <NotificationModal /> */}
       <StudentHomePage />
     </div >
   )

bfa3f11814c37533dcf0d1433f9b75a89caa4d82
Noman Shamim <nomanshamim2004@gmail.com>
2024-07-19 21:34:23 +0500

    AssignmentSubmitForm Modal done.

--- front-end/src/Components/Pages/Home/Home.jsx
+++ front-end/src/Components/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Footer from '../../Footer/Footer'
 import StudentHomePage from '../Students/StudentHomePage'
 import NotificationModal from '../../NotificationModal/NotificationModal'
 import StudentUpdateProfilePage from '../Students/StudentUpdateProfilePage'
+import AssignmentSubmitFormModal from '../../AssignmentSubmitFormModal/AssignmentSubmitFormModal'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       {/* <NotificationModal /> */}
       <StudentHomePage />
       {/* <StudentUpdateProfilePage /> */}
+      <AssignmentSubmitFormModal />
     </div >
   )
 }

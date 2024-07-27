ceddf7e06a18db938e513b91af55efaa76354c2a
Noman Shamim <nomanshamim2004@gmail.com>
2024-07-27 21:47:54 +0500

    Allassignmentlisting page UI done.

--- front-end/src/Components/Pages/Teachers/AllAssignmentListing.jsx
+++ front-end/src/Components/Pages/Teachers/AllAssignmentListing.jsx
@@ -1,7 +1,105 @@
 import { BellFilled } from '@ant-design/icons'
-import { Button, Progress } from 'antd'
+import { Space, Table, Tag } from 'antd'
+import { FaEdit } from "react-icons/fa";
+import { FaDeleteLeft } from "react-icons/fa6";
 import React from 'react'
 import { Container } from 'react-bootstrap'
+
+const columns = [
+    {
+        title: 'S/No',
+        dataIndex: 'serialNo',
+        key: 'serialNo',
+        width: 100,
+        render: (number) => <a>{number}</a>,
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text) => <a>{text}</a>,
+    },
+    {
+        title: 'Topic',
+        dataIndex: 'topic',
+        key: 'address',
+    },
+    {
+        title: 'Due date',
+        dataIndex: 'date',
+        key: 'date',
+    },
+    {
+        title: 'Status',
+        key: 'tags',
+        dataIndex: 'tags',
+        render: (_, { tags }) => (
+            <>
+                {tags.map((tag) => {
+                    let color = tag.length > 5 ? 'geekblue' : 'green';
+                    if (tag === 'expired') {
+                        color = 'volcano';
+                    }
+                    if (tag === 'submitted') {
+                        color = 'geekblue'
+                    }
+                    if (tag === 'pending') {
+                        color = 'green'
+                    }
+                    return (
+                        <Tag color={color} key={tag}>
+                            {tag.toUpperCase()}
+                        </Tag>
+                    );
+                })}
+            </>
+        ),
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: (_, record) => (
+            <Space size="middle">
+                <a className='text-xl hover:text-green-500'><FaEdit /></a>
+                <a className='text-xl hover:text-red-500'><FaDeleteLeft /></a>
+            </Space>
+        ),
+    },
+];
+const data = [
+    {
+        key: '1',
+        serialNo: 1,
+        name: 'Assignment',
+        date: '10-05-24',
+        topic: 'Html , Css',
+        tags: ['submitted'],
+    },
+    {
+        key: '2',
+        serialNo: 2,
+        name: 'Assignment',
+        date: '12-05-24',
+        topic: 'JavaScript , React JS',
+        tags: ['expired'],
+    },
+    {
+        key: '3',
+        serialNo: 3,
+        name: 'Assignment',
+        date: '14-05-24',
+        topic: 'Express JS , MongoDB',
+        tags: ['pending'],
+    },
+    {
+        key: '4',
+        serialNo: 4,
+        name: 'Assignment',
+        date: '16-05-24',
+        topic: 'Node JS',
+        tags: ['submitted'],
+    },
+];
 
 export default function AllAssignmentListing() {
     return (
@@ -14,7 +112,7 @@ export default function AllAssignmentListing() {
 
                 <div className='mt-8'>
 
-                    <div className='flex justify-between my-4'>
+                    <div className='flex justify-between m-4'>
                         <div>
                             <h1 className='font-bold text-lg'>All Assignments</h1>
                             <p className='text-xs'>View and manage all assignments</p>
@@ -24,75 +122,8 @@ export default function AllAssignmentListing() {
                         </div>
                     </div>
 
-                    <div className='flex gap-4 flex-col'>
-
-                        <div className='flex justify-between p-3 rounded-md bg-gray-200 shadow-xl font-bold'>
-                            <div>
-                                <h1>Name</h1>
-                            </div>
-                            <div><h1>Due Date</h1></div>
-                            <div>
-                                <h1>Status</h1>
-                            </div>
-
-                            <div>
-                                <h1>Actions</h1>
-                            </div>
-                        </div>
-
-                        <div className='flex justify-between p-3 rounded-md bg-gray-200 shadow-xl'>
-                            <div>
-                                <p className='font-serif'>Assignment 1</p>
-                                <p className='text-xs font-mono text-sky-600'>description 1</p>
-                            </div>
-                            <div><p>25-08-2024</p></div>
-                            <div>
-                                <p>Pending</p>
-                            </div>
-
-                            <div>
-                                <Button type='text' className='border-none'>
-                                    View Overall Report
-                                </Button>
-                            </div>
-                        </div>
-
-                        <div className='flex justify-between p-3 rounded-md bg-gray-200 shadow-xl'>
-                            <div>
-                                <p className='font-serif'>Assignment 2</p>
-                                <p className='text-xs font-mono text-sky-600'>descrption 2</p>
-                            </div>
-                            <div><p>27-08-2024</p></div>
-                            <div>
-                                <p>Expired</p>
-                            </div>
-
-                            <div>
-                                <Button type='text' className='border-none'>
-                                    View Overall Report
-                                </Button>
-                            </div>
-                        </div>
-
-                        <div className='flex justify-between p-3 rounded-md bg-gray-200 shadow-xl'>
-                            <div>
-                                <p className='font-serif'>Assignment 3</p>
-                                <p className='text-xs font-mono text-sky-600'>descrption 3</p>
-                            </div>
-                            <div><p>30-08-2024</p></div>
-                            <div>
-                                <p>Submitted</p>
-                            </div>
-
-                            <div>
-                                <Button type='text' className='border-none'>
-                                    View Overall Report
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
+                    <Table columns={columns} dataSource={data} />
                 </div>
-
 
             </Container>
         </div>

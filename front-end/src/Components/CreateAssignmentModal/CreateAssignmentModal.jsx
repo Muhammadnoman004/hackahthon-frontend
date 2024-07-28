bf945601de29f5e26153e615fbd9372e5f6dd514
Noman Shamim <nomanshamim2004@gmail.com>
2024-07-28 23:36:00 +0500

    CreateAssignmentModal done.

--- front-end/src/Components/CreateAssignmentModal/CreateAssignmentModal.jsx
+++ front-end/src/Components/CreateAssignmentModal/CreateAssignmentModal.jsx
@@ -1,39 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Input, Modal } from 'antd';
-import { FaUserLock } from "react-icons/fa6";
-import { RiLockPasswordFill } from "react-icons/ri";
+import { MdAssignment, MdEditNote } from "react-icons/md";
 
 export default function CreateAssignmentModal() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <>
-            <Button type="primary">
+            <Button type="primary" onClick={showModal}>
                 Create Assignment
             </Button>
-            <Modal footer={null} open>
+            <Modal footer={null} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
 
-                <div className='p-2'>
+                <div className='p-3'>
                     <div>
-                        <h1 className='text-lg text-center font-bold uppercase'>User Inforamtion</h1>
+                        <h1 className='text-lg font-bold uppercase'>Create New Assignment</h1>
+                        <p className='text-xs'>Fill out the form to create a new assignment.</p>
                     </div>
 
                     <div>
                         <div className='my-4 text-base'>
 
-                            <label>Current Password</label>
-                            <Input size="large" placeholder="Current Password" prefix={<FaUserLock />} className='mb-5' />
+                            <label className='font-semibold'>Name</label>
+                            <Input size="large" placeholder="Enter assignment name" prefix={<MdAssignment />} className='mb-4' />
 
-                            <label>New Password</label>
-                            <Input size="large" type='password' placeholder="New Password" prefix={<RiLockPasswordFill />} className='mb-5' />
+                            <label className='font-semibold'>Description</label>
+                            <Input.TextArea size="large" type='password' placeholder="Enter assignment description" prefix={<MdEditNote />} className='mb-4 ' />
 
-                            <label>Confirm Password</label>
-                            <Input size="large" type='password' placeholder="Confirm Password" prefix={<RiLockPasswordFill />} className='mb-5' />
+                            <label className='font-semibold'>Due Date</label>
+                            <Input size="large" type='date' placeholder="Confirm Password" className='mb-5' />
 
                         </div>
                     </div>
 
                     <div>
-                        <Button type='primary' danger >Cancel</Button>
-                        <Button type='primary' className='mx-2'>Edit</Button>
+                        <Button type='primary' danger onClick={handleCancel}>Cancel</Button>
+                        <Button type='primary' className='mx-2' onClick={handleOk}>Submit</Button>
                     </div>
 
                 </div>

c15026d0f9f9da5e1f0a249050a3cd3d2dc01484
Noman Shamim <nomanshamim2004@gmail.com>
2024-07-29 21:13:25 +0500

    Working on AllStudentGradePage UI,,,

--- front-end/src/Components/Pages/Home/Home.jsx
+++ front-end/src/Components/Pages/Home/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
       {/* <AssignmentSubmitFormModal /> */}
       {/* <TeacherStudentPage /> */}
       <AllAssignmentListing />
-      <CreateAssignmentModal />
+      {/* <CreateAssignmentModal /> */}
     </div >
   )
 }
---
+++ front-end/src/Components/Pages/Teachers/AllStudentGradePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AllStudentGradePage() {
+    return (
+        <div>
+            <h1>AllStudentGradePage</h1>
+        </div>
+    )
+}

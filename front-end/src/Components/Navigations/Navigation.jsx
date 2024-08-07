8a52bff3e2e748205c5cc0f719ea5aa87e786492
Noman Shamim <nomanshamim2004@gmail.com>
2024-08-08 00:42:27 +0500

    Add some routes...

--- front-end/src/Components/Navigations/Navigation.jsx
+++ front-end/src/Components/Navigations/Navigation.jsx
@@ -10,6 +10,9 @@ import AllClassFellowsPage from '../Pages/Students/AllClassFellowsPage';
 import StudentSettingPage from '../Pages/Students/StudendSettingPage';
 import Navbars from '../Navbars/Navbars';
 import TeacherHomePage from '../Pages/Teachers/TeacherHomePage';
+import AdminHomePage from '../Pages/Admin/AdminHomePage';
+import AllTeacherPage from '../Pages/Admin/AllTeacherPage';
+import AllStudentPage from '../Pages/Admin/AllStudentPage';
 
 const router = createBrowserRouter([
     {
@@ -24,26 +27,47 @@ const router = createBrowserRouter([
         path: "/login",
         element: <Login />
     },
+
+    // Students Routes
+
     {
         path: "/student/home",
-        element: <StudentHomePage />
+        element: <>
+            <Navbars />
+            <StudentHomePage />
+        </>
     },
     {
         path: "/student/assignment/listing",
-        element: <StudentAllAssignmentPage />
+        element: <>
+            <Navbars />
+            <StudentAllAssignmentPage />
+        </>
     },
     {
         path: "/student/assignment/todo",
-        element: <StudentAssignmentTodoPage />
+        element: <>
+            <Navbars />
+            <StudentAssignmentTodoPage />
+        </>
     },
     {
         path: "/student/classfellows",
-        element: <AllClassFellowsPage />
+        element: <>
+            <Navbars />
+            <AllClassFellowsPage />
+        </>
     },
     {
         path: "/student/setting",
-        element: <StudentSettingPage />
+        element: <>
+            <Navbars />
+            <StudentSettingPage />
+        </>
     },
+
+    // Trainer Routes
+
     {
         path: "/trainer/dashboard",
         element: <>
@@ -51,6 +75,31 @@ const router = createBrowserRouter([
             < TeacherHomePage />
         </>
     },
+
+    // Admin Routes
+
+    {
+        path: "/admin/dashboard",
+        element: <>
+            <Navbars />
+            < AdminHomePage />
+        </>
+    },
+    {
+        path: "/admin/teacher",
+        element: <>
+            <Navbars />
+            < AllTeacherPage />
+        </>
+    },
+    {
+        path: "/admin/student",
+        element: <>
+            <Navbars />
+            < AllStudentPage />
+        </>
+    },
+
 ])
 
 

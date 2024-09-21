f23bd9c9eb95158023b596703df23befd67de79b
Noman Shamim <nomanshamim2004@gmail.com>
2024-09-21 20:57:08 +0500

    start working on teachers API  on admin side

--- front-end/src/Components/Pages/Admin/AllTeacherPage.jsx
+++ front-end/src/Components/Pages/Admin/AllTeacherPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Form, Input, Modal, Radio } from 'antd';
 import { Container } from 'react-bootstrap'
 import { Space, Table } from 'antd';
 import { FaEdit } from 'react-icons/fa';
@@ -61,6 +62,17 @@ const data = [
 
 
 export default function AllTeacherPage() {
+
+    const [form] = Form.useForm();
+    const [formValues, setFormValues] = useState();
+    const [open, setOpen] = useState(false);
+    const onCreate = (values) => {
+        console.log('Received values of form: ', values);
+        setFormValues(values);
+        setOpen(false);
+    };
+
+
     return (
         <div>
             <Container>
@@ -69,12 +81,85 @@ export default function AllTeacherPage() {
                         <h1 className='font-bold text-xl'>All Teachers</h1>
                     </div>
                     <div>
-                        <button className='p-1 px-3 w-auto  bg-sky-blue text-white rounded-md border-none hover:bg-sky-400 focus:shadow-lg'>Add Teacher</button>
+                        <button className='p-1 px-3 w-auto  bg-sky-blue text-white rounded-md border-none hover:bg-sky-400 focus:shadow-lg' onClick={() => setOpen(true)}>Add Teacher</button>
                     </div>
                 </div>
 
                 <Table className='shadow-xl mb-5' bordered columns={columns} dataSource={data} />
 
+
+                <Modal
+                    open={open}
+                    title="Add Teacher"
+                    okText="Add"
+                    cancelText="Cancel"
+                    okButtonProps={{
+                        autoFocus: true,
+                        htmlType: 'submit',
+                    }}
+                    onCancel={() => setOpen(false)}
+                    destroyOnClose
+                    modalRender={(dom) => (
+                        <Form
+                            layout="vertical"
+                            form={form}
+                            name="form_in_modal"
+                            initialValues={{
+                                modifier: 'public',
+                            }}
+                            clearOnDestroy
+                            onFinish={(values) => onCreate(values)}
+                        >
+                            {dom}
+                        </Form>
+                    )}
+                >
+                    <Form.Item
+                        name="name"
+                        label="Teacher Name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter teacher name!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name="email"
+                        label="Email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter teacher email!',
+                            },
+                            {
+                                type: 'email',
+                                message: 'Please enter a valid email!'
+                            }
+
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        label="Password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter teacher password!',
+                            },
+                            {
+                                min: 6,
+                                message: 'Password must be at least 6 characters!'
+                            }
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                </Modal>
             </Container>
         </div>
     )

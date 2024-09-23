87631a9f1a1c98925256d8dc60d9b9aed894272e
Noman Shamim <nomanshamim2004@gmail.com>
2024-09-24 01:27:41 +0500

    Still working...

--- front-end/src/Components/Pages/Admin/AllTeacherPage.jsx
+++ front-end/src/Components/Pages/Admin/AllTeacherPage.jsx
@@ -7,60 +7,32 @@ import { FaDeleteLeft } from 'react-icons/fa6';
 import api from '../../../api/api';
 import { toast } from 'react-toastify';
 
-const columns = [
-    {
-        title: 'S/No',
-        dataIndex: 'serialNo',
-        key: 'serialNo',
-        width: '10%',
-        render: (number) => <a>{number}</a>,
-        sorter: (a, b) => a.serialNo - b.serialNo,
-    },
-    {
-        title: 'Teacher Name',
-        dataIndex: 'name',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-    },
-    {
-        title: 'Action',
-        dataIndex: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <a className='text-xl hover:text-green-500'><FaEdit /></a>
-                <a className='text-xl hover:text-red-500'><FaDeleteLeft /></a>
-            </Space>
-        ),
-    },
-];
-const data = [
-    {
-        key: '1',
-        serialNo: '01',
-        name: 'Jamsheed',
-        email: 'Jamsheed@gmail.com',
-    },
-    {
-        key: '2',
-        serialNo: '02',
-        name: 'Huzaifa',
-        email: 'Huzaifa@gmail.com',
-    },
-    {
-        key: '3',
-        serialNo: '03',
-        name: 'Basit',
-        email: 'Basit@gmail.com',
-    },
-    {
-        key: '4',
-        serialNo: '04',
-        name: 'Tayyab',
-        email: 'Tayyab@gmail.com',
-    },
-];
+// const data = [
+//     {
+//         key: '1',
+//         serialNo: '01',
+//         name: 'Jamsheed',
+//         email: 'Jamsheed@gmail.com',
+//     },
+//     {
+//         key: '2',
+//         serialNo: '02',
+//         name: 'Huzaifa',
+//         email: 'Huzaifa@gmail.com',
+//     },
+//     {
+//         key: '3',
+//         serialNo: '03',
+//         name: 'Basit',
+//         email: 'Basit@gmail.com',
+//     },
+//     {
+//         key: '4',
+//         serialNo: '04',
+//         name: 'Tayyab',
+//         email: 'Tayyab@gmail.com',
+//     },
+// ];
 
 
 export default function AllTeacherPage() {
@@ -102,15 +74,49 @@ export default function AllTeacherPage() {
     const getAllTeachers = async () => {
         try {
             const res = await api.get("/api/users/trainers")
-            console.log(res.data);
-            setTeachers(res.data);
+            const teachersWithSerial = res.data.map((teacher, index) => ({
+                ...teacher,
+                serialNo: index + 1,
+                key: teacher._id
+            }))
+            setTeachers(teachersWithSerial);
+            console.log(teachersWithSerial);
 
         } catch (error) {
             console.log(error);
-
         }
     }
 
+    const columns = [
+        {
+            title: 'S/No',
+            dataIndex: 'serialNo',
+            key: 'serialNo',
+            width: '10%',
+            render: (number) => <span>{number}</span>,
+            sorter: (a, b) => a.serialNo - b.serialNo,
+        },
+        {
+            title: 'Teacher Name',
+            dataIndex: 'username',
+            key: 'username'
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email'
+        },
+        {
+            title: 'Action',
+            dataIndex: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a className='text-xl hover:text-green-500'><FaEdit /></a>
+                    <a className='text-xl hover:text-red-500'><FaDeleteLeft /></a>
+                </Space>
+            ),
+        },
+    ];
 
     return (
         <div>
@@ -124,7 +130,12 @@ export default function AllTeacherPage() {
                     </div>
                 </div>
 
-                <Table className='shadow-xl mb-5' bordered columns={columns} dataSource={data} />
+                <Table
+                    className='shadow-xl mb-5'
+                    bordered
+                    columns={columns}
+                    dataSource={teachers}
+                />
 
 
                 <Modal

2b58af2d98baa1a28bce03394fac8d2c2ab901b5
Noman Shamim <nomanshamim2004@gmail.com>
2024-10-14 21:59:07 +0500

    working add student on admin side.

--- front-end/src/Components/Pages/Admin/AllStudentPage.jsx
+++ front-end/src/Components/Pages/Admin/AllStudentPage.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
-import { Space, Table } from 'antd';
+import { Space, Table, Form, Input, Modal } from 'antd';
 import { FaEdit } from 'react-icons/fa';
 import { FaDeleteLeft } from 'react-icons/fa6';
 
@@ -85,6 +85,10 @@ const data = [
 
 
 export default function AllStudentPage() {
+
+    const [form] = Form.useForm();
+    const [open, setOpen] = useState(false);
+
     return (
         <div>
             <Container>
@@ -93,11 +97,86 @@ export default function AllStudentPage() {
                         <h1 className='font-bold text-xl'>All Students</h1>
                     </div>
                     <div>
-                        <button className='p-1 px-3 w-auto  bg-sky-blue text-white rounded-md border-none hover:bg-sky-400 focus:shadow-lg'>Add Student</button>
+                        <button className='p-1 px-3 w-auto  bg-sky-blue text-white rounded-md border-none hover:bg-sky-400 focus:shadow-lg' onClick={() => setOpen(true)}>Add Student</button>
                     </div>
                 </div>
 
                 <Table className='shadow-xl mb-5' bordered columns={columns} dataSource={data} />
+
+
+                <Modal
+                    open={open}
+                    title="Add Teacher"
+                    okText="Add"
+                    cancelText="Cancel"
+                    okButtonProps={{
+                        autoFocus: true,
+                        htmlType: 'submit',
+                    }}
+                    onCancel={() => setOpen(false)}
+                    destroyOnClose
+                    modalRender={(dom) => (
+                        <Form
+                            layout="vertical"
+                            form={form}
+                            name="form_in_modal"
+                            initialValues={{
+                                modifier: 'public',
+                            }}
+                            clearOnDestroy
+                            onFinish={(values) => handleAddTeacher(values)}
+                        >
+                            {dom}
+                        </Form>
+                    )}
+                >
+                    <Form.Item
+                        name="name"
+                        label="Student Name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter student name!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name="email"
+                        label="Email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter student email!',
+                            },
+                            {
+                                type: 'email',
+                                message: 'Please enter a valid email!'
+                            }
+
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        label="Password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please enter teacher password!',
+                            },
+                            {
+                                min: 6,
+                                message: 'Password must be at least 6 characters!'
+                            }
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                </Modal>
+
 
             </Container>
         </div>

7956fa98db56c1d399ecbcdd2f0c63780380a345
Noman Shamim <nomanshamim2004@gmail.com>
2024-10-27 21:18:35 +0500

    working on Create Class modal....

--- front-end/src/Components/Pages/Teachers/TeacherHomePage.jsx
+++ front-end/src/Components/Pages/Teachers/TeacherHomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BellFilled } from '@ant-design/icons'
 import { Container } from 'react-bootstrap'
+import { AiOutlinePlus } from "react-icons/ai";
 
 export default function TeacherHomePage() {
     return (
@@ -8,10 +9,17 @@ export default function TeacherHomePage() {
             <Container>
                 <div className='flex m-4 text-2xl font-mono font-extrabold'>
                     <h1 className='flex-1'>Trainer Dashboard</h1>
-                    <BellFilled className='flex-2 text-amber-400' />
+                    <div className='flex gap-3'>
+                        <div className='hover:rounded-full hover:bg-slate-200 cursor-pointer font-bold'>
+                            <AiOutlinePlus className='text-3xl p-0.5' />
+                        </div>
+                        <div>
+                            <BellFilled className='flex text-amber-400' />
+                        </div>
+                    </div>
                 </div>
-                <div className='m-4 text-2xl font-serif font-semibold'>
-                    <h1>Welcome Noman,</h1>
+                <div className='m-4 text-2xl font-sans font-bold text-sky-blue'>
+                    <h2>My Classes</h2>
                 </div>
             </Container>
         </div>

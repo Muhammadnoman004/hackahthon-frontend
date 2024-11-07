315a0ca6b2a76ec11ac2a395ee5f3928ebafe7b0
Noman Shamim <nomanshamim2004@gmail.com>
2024-11-08 01:11:38 +0500

    getAllClass render in browser from db..

--- front-end/src/Components/Pages/Teachers/TeacherHomePage.jsx
+++ front-end/src/Components/Pages/Teachers/TeacherHomePage.jsx
@@ -76,47 +76,34 @@ export default function TeacherHomePage() {
 
                     <Row>
                         <Col xs={12} md={6} lg={4}>
-                            <Card
-                                hoverable
-                                style={{
-                                    width: 300,
-                                    margin: 'auto',
-                                    marginBottom: 30
-                                }}
-                                cover={<img alt="example" className='size-36' style={{ borderRadius: "10px" }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPIQRaKMkaQh1KFCfy8Xmz4H_Jy3waZw2mw&s" />}
-                            >
-                                <div className='flex relative bottom-12'>
-                                    <h1 className='flex-1 relative top-8 right-3 font-semibold'>Web development</h1>
-                                    <img className='size-12 rounded-full bg-white' src={userProfileIcon} alt="" />
-                                </div>
+                            {loading ?
+                                <Card loading={loading}></Card> : classes.length === 0 ? (
+                                    <div>You haven't created any class yet!</div>
+                                ) : classes.map((eachClass, index) => (
+                                    <div key={index}>
 
-                                <div className='flex'>
-                                    <Meta title="Batch-10" className='flex-1 relative right-3' />
-                                    <Meta title="Sir Saad" className='relative left-3' />
-                                </div>
-                            </Card>
-                        </Col>
+                                        <Card
+                                            hoverable
+                                            style={{
+                                                width: 300,
+                                                margin: 'auto',
+                                                marginBottom: 30
+                                            }}
+                                            cover={<img alt="example" className='size-36' style={{ borderRadius: "10px" }} src={eachClass.classImage} />}
+                                        >
+                                            <div className='flex relative bottom-12'>
+                                                <h1 className='flex-1 relative top-8 right-3 font-semibold'>{eachClass.description}</h1>
+                                                <img className='size-12 rounded-full bg-white' src={userProfileIcon} alt="" />
+                                            </div>
 
-                        <Col xs={12} md={6} lg={4}>
-                            <Card
-                                hoverable
-                                style={{
-                                    width: 300,
-                                    margin: 'auto',
-                                    marginBottom: 30
-                                }}
-                                cover={<img alt="example" className='size-36' style={{ borderRadius: "10px" }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPIQRaKMkaQh1KFCfy8Xmz4H_Jy3waZw2mw&s" />}
-                            >
-                                <div className='flex relative bottom-12'>
-                                    <h1 className='flex-1 relative top-8 right-3 font-semibold'>Python development</h1>
-                                    <img className='size-12 rounded-full bg-white' src={userProfileIcon} alt="" />
-                                </div>
-
-                                <div className='flex'>
-                                    <Meta title="Batch-8" className='flex-1 relative right-3' />
-                                    <Meta title="Sir Huzaifa" className='relative left-3' />
-                                </div>
-                            </Card>
+                                            <div className='flex'>
+                                                <Meta title={eachClass.name} className='flex-1 relative right-3' />
+                                                <Meta title="Sir Saad" className='relative left-3' />
+                                            </div>
+                                        </Card>
+                                    </div>
+                                )
+                                )}
                         </Col>
 
                         <Col xs={12} md={6} lg={4}>

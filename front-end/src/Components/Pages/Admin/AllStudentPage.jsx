bac56f00a14e98b1407b4f9ec6f216ce94f73200
Noman Shamim <nomanshamim2004@gmail.com>
2024-11-20 23:18:15 +0500

    deleted some extra code...

--- front-end/src/Components/Pages/Admin/AllStudentPage.jsx
+++ front-end/src/Components/Pages/Admin/AllStudentPage.jsx
@@ -9,52 +9,6 @@ import loader from '../../../Context/LoaderContext';
 import Loader from '../../Loader/Loader';
 
 
-// const data = [
-//     {
-//         key: '1',
-//         serialNo: '01',
-//         name: 'Asad',
-//         email: 'Asad@gmail.com',
-//         enrolled: 0
-//     },
-//     {
-//         key: '2',
-//         serialNo: '02',
-//         name: 'Talha',
-//         email: 'Talha@gmail.com',
-//         enrolled: 2
-//     },
-//     {
-//         key: '3',
-//         serialNo: '03',
-//         name: 'Saad',
-//         email: 'Saad@gmail.com',
-//         enrolled: 1
-//     },
-//     {
-//         key: '4',
-//         serialNo: '04',
-//         name: 'Khan',
-//         email: 'Khan@gmail.com',
-//         enrolled: 0
-//     },
-//     {
-//         key: '5',
-//         serialNo: '05',
-//         name: 'Hamza',
-//         email: 'Hamza@gmail.com',
-//         enrolled: 2
-//     },
-//     {
-//         key: '6',
-//         serialNo: '06',
-//         name: 'Aman',
-//         email: 'Aman@gmail.com',
-//         enrolled: 0
-//     },
-// ];
-
-
 export default function AllStudentPage() {
 
     const [form] = Form.useForm();

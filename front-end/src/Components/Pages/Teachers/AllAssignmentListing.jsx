cf2b1f5b7ac818c5a3ed89e6efac5d95ba5115b7
Noman Shamim <nomanshamim2004@gmail.com>
2025-01-08 23:35:24 +0500

    working...

--- front-end/src/Components/Pages/Teachers/AllAssignmentListing.jsx
+++ front-end/src/Components/Pages/Teachers/AllAssignmentListing.jsx
@@ -1,9 +1,7 @@
-import { BellFilled } from '@ant-design/icons'
-import { Space, Table, Tag } from 'antd'
-import { FaEdit } from "react-icons/fa";
-import { FaDeleteLeft } from "react-icons/fa6";
-import React from 'react'
+import { Space, Table } from 'antd'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
+import CreateAssignmentModal from '../../CreateAssignmentModal/CreateAssignmentModal';
 
 const columns = [
     {
@@ -25,40 +23,21 @@ const columns = [
         key: 'date',
     },
     {
-        title: 'Status',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (_, { tags }) => (
-            <>
-                {tags.map((tag) => {
-                    let color = tag.length > 5 ? 'geekblue' : 'green';
-                    if (tag === 'expired') {
-                        color = 'volcano';
-                    }
-                    if (tag === 'submitted') {
-                        color = 'geekblue'
-                    }
-                    if (tag === 'pending') {
-                        color = 'green'
-                    }
-                    return (
-                        <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </>
-        ),
+        title: 'Total Marks',
+        key: 'marks',
+        dataIndex: 'marks',
     },
     {
         title: 'Action',
         key: 'action',
         render: (_, record) => (
             <Space size="small">
-                <a className='text-sm text-black border-dashed border-2 border-teal-600 p-2 rounded-md hover:bg-teal-600'>View Assignment</a>
-                <a className='text-sm text-white bg-teal-600 p-2 rounded-md hover:bg-teal-700'>View Submissions</a>
-                <a className='text-sm text-white bg-green-600 p-2 rounded-md hover:bg-green-700'>Edit</a>
-                <a className='text-sm text-white bg-red-600 p-2 rounded-md hover:bg-red-700'>Delete</a>
+                <div className='flex justify-between items-center gap-2'>
+                    <button className='text-sm text-black border-dashed border-2 border-teal-600 p-2 rounded-md hover:bg-teal-600 duration-500 transition-all'>View Assignment</button>
+                    <button className='text-sm text-white bg-teal-600 p-2 rounded-md hover:bg-teal-700'>View Submissions</button>
+                    <button className='text-sm text-white bg-green-600 p-2 rounded-md hover:bg-green-700'>Edit</button>
+                    <button className='text-sm text-white bg-red-600 p-2 rounded-md hover:bg-red-700'>Delete</button>
+                </div>
             </Space>
         ),
     },
@@ -69,32 +48,35 @@ const data = [
         serialNo: 1,
         name: 'Assignment',
         date: '10-05-24',
-        tags: ['submitted'],
+        marks: 100
     },
     {
         key: '2',
         serialNo: 2,
         name: 'Assignment',
         date: '12-05-24',
-        tags: ['expired'],
+        marks: 10
     },
     {
         key: '3',
         serialNo: 3,
         name: 'Assignment',
         date: '14-05-24',
-        tags: ['pending'],
+        marks: 50
     },
     {
         key: '4',
         serialNo: 4,
         name: 'Assignment',
         date: '16-05-24',
-        tags: ['submitted'],
+        marks: 20
     },
 ];
 
 export default function AllAssignmentListing() {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
         <div>
             <Container>
@@ -106,7 +88,7 @@ export default function AllAssignmentListing() {
                             <p className='text-xs'>View and manage all assignments</p>
                         </div>
                         <div>
-                            <button className='p-1 px-3 w-auto  bg-sky-blue text-white rounded-md border-none hover:bg-sky-400 focus:shadow-lg'>Create Assignment</button>
+                            <button className='p-1 px-3 w-auto  bg-sky-blue text-white rounded-md border-none hover:bg-sky-400 focus:shadow-lg' onClick={() => setIsModalOpen(true)}>Create Assignment</button>
                         </div>
                     </div>
 
@@ -114,6 +96,8 @@ export default function AllAssignmentListing() {
                 </div>
 
             </Container>
+
+            <CreateAssignmentModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
         </div>
     )
 }

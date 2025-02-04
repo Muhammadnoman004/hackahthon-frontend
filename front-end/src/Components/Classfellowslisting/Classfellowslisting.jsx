bf9bc38636b16405fd4025ac78b942d131fb268b
Noman Shamim <nomanshamim2004@gmail.com>
2025-02-04 21:47:38 +0500

    classfellows listing done on trainerside.

--- front-end/src/Components/Classfellowslisting/Classfellowslisting.jsx
+++ front-end/src/Components/Classfellowslisting/Classfellowslisting.jsx
@@ -1,55 +1,46 @@
 import React from 'react'
 import UserProIcon from '../../assets/user-profile-icon.png';
 
-export default function Classfellowslisting() {
+export default function Classfellowslisting({ data, classId }) {
     return (
         <div>
             <div className="bg-white rounded-lg border shadow-md w-full max-w-3xl mt-4 p-4">
-                <div className='border-b-2 pb-3 w-full flex gap-3'>
-                    <div className='w-14 h-14 rounded-full bg-stone-200'>
-                        <img src={UserProIcon} alt="" />
-                    </div>
-                    <div >
-                        <h2 className='text-xl font-semibold'>Asad Sheikh</h2>
-                        <p className='text-sm'>asad@gmail.com</p>
-                    </div>
-                </div>
+                {
+                    (data && classId) ?
+                        data.map((data, i) => {
+                            return (
+                                <div key={i}>
+                                    <div className='border-b-2 pb-3 w-full flex gap-3 mb-3'>
+                                        <div className='w-14 h-14 rounded-full bg-stone-200 overflow-hidden'>
+                                            <img src={data.profileImg ? data.profileImg : UserProIcon} alt="" />
+                                        </div>
+                                        <div >
+                                            <h2 className='text-xl font-semibold'>{data.username}</h2>
+                                            <p className='text-sm'>{data.email}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }) :
+                        data.map((data, i) => {
+                            return (
+                                <div key={i}>
+                                    <div className='border-b-2 pb-3 w-full flex gap-3 mb-3'>
+                                        <div className='w-14 h-14 rounded-full bg-stone-200 overflow-hidden'>
+                                            <img src={data.profileImg ? data.profileImg : UserProIcon} alt="" />
+                                        </div>
+                                        <div >
+                                            <h2 className='text-xl font-semibold'>{data.username}</h2>
+                                            <p className='text-sm'>{data.email}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                }
+
             </div>
-
             <div>
-
-                <div className="bg-white rounded-lg border shadow-md w-full max-w-3xl mt-4 p-4">
-                    <div className='border-b-2 pb-3 w-full flex gap-3 mb-3'>
-                        <div className='w-14 h-14 rounded-full bg-stone-200'>
-                            <img src={UserProIcon} alt="" />
-                        </div>
-                        <div >
-                            <h2 className='text-xl font-semibold'>Asad Sheikh</h2>
-                            <p className='text-sm'>asad@gmail.com</p>
-                        </div>
-                    </div>
-
-                    <div className='border-b-2 pb-3 w-full flex gap-3 mb-3'>
-                        <div className='w-14 h-14 rounded-full bg-stone-200'>
-                            <img src={UserProIcon} alt="" />
-                        </div>
-                        <div >
-                            <h2 className='text-xl font-semibold'>Jamsheed</h2>
-                            <p className='text-sm'>Jamsheed@gmail.com</p>
-                        </div>
-                    </div>
-
-                    <div className='border-b-2 pb-3 w-full flex gap-3 mb-3'>
-                        <div className='w-14 h-14 rounded-full bg-stone-200'>
-                            <img src={UserProIcon} alt="" />
-                        </div>
-                        <div >
-                            <h2 className='text-xl font-semibold'>Bilal</h2>
-                            <p className='text-sm'>Bilal@gmail.com</p>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
     )
--- front-end/src/Components/Pages/Teachers/AllClassFellowsPage.jsx
+++ front-end/src/Components/Pages/Teachers/AllClassFellowsPage.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import useFetchProfile from '../../../utils/useFetchProfile';
 import { toast } from 'react-toastify';
 import loader from '../../../Context/LoaderContext';
+import Classfellowslisting from '../../Classfellowslisting/Classfellowslisting';
 
 export default function AllClassFellowsPage() {
 
@@ -35,10 +36,16 @@ export default function AllClassFellowsPage() {
 
     return (
         <div>
-            <div className='flex text-2xl font-extrabold mb-4'>
+            <div className='flex text-2xl font-extrabold mb-4 mt-4'>
                 <div className='flex-1'>
                     <h3 className="whitespace-nowrap text-2xl font-semibold leading-none tracking-tight mb-1">Teacher</h3>
                 </div>
+            </div>
+
+            <div className='mb-4'>
+                {
+                    trainerData && <Classfellowslisting data={trainerData} />
+                }
             </div>
 
             <div className='flex text-2xl font-extrabold mb-4'>
@@ -46,6 +53,10 @@ export default function AllClassFellowsPage() {
                     <h3 className="whitespace-nowrap text-2xl font-semibold leading-none tracking-tight mb-1">Classmates</h3>
                     <p className='text-sm font-normal'>Get to know your fellow students!</p>
                 </div>
+            </div>
+
+            <div className='mb-4'>
+                <Classfellowslisting data={studentsData} classId={classId} />
             </div>
 
         </div>

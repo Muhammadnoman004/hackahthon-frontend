45fde5ab2993d165382b100a40b266da43092c7b
Noman Shamim <nomanshamim2004@gmail.com>
2025-03-12 19:41:10 +0500

    working on assignment detail page on student side....

--- front-end/src/Components/Pages/Students/StudentAssignmentDetailPage.jsx
+++ front-end/src/Components/Pages/Students/StudentAssignmentDetailPage.jsx
@@ -1,21 +1,214 @@
-import { Alert, Spin } from 'antd'
-import React from 'react'
+import { Alert, Button, Dropdown, Input, Modal, Progress, Space, Spin, Tag, Upload } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { FaArrowLeft, FaPlus } from 'react-icons/fa'
+import { FaDownload } from 'react-icons/fa6'
+import { IoLink } from 'react-icons/io5'
+import { LiaClipboardListSolid } from 'react-icons/lia'
+import { MdAttachFile } from 'react-icons/md'
+import api from '../../../api/api'
+import { useParams } from 'react-router-dom'
+import useFetchProfile from '../../../utils/useFetchProfile'
 
 function StudentAssignmentDetailPage() {
+
+    const { user } = useFetchProfile();
+    const { classId, assignmentId } = useParams();
+    const [submitModalVisible, setSubmitModalVisible] = useState(false);
+
+    useEffect(() => {
+        fetchAssignmentReport();
+    }, [assignmentId])
+
+    const fetchAssignmentReport = async () => {
+        try {
+            const userId = localStorage.getItem('userId');
+            const response = await api.get(`/api/assignments/${assignmentId}/report/${userId}`);
+            console.log(response.data);
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    const showSubmitModal = () => setSubmitModalVisible(true);
+    const handleSubmitCancel = () => {
+        setSubmitModalVisible(false);
+    }
+
+    const items = [
+        {
+            key: 'link',
+            label: (
+                <p>
+                    Link
+                </p>
+            ),
+            icon: <IoLink />,
+        },
+        {
+            key: 'file',
+            label: (<p>File</p>),
+            icon: <MdAttachFile />,
+        },
+    ];
+
+    const onClick = ({ key }) => {
+        showSubmitModal()
+    }
+
     return (
-        <div>
-            <div className='flex justify-center items-center h-screen'>
-                <Spin size='large' />
-            </div>
+        <div className='p-4'>
+            <Container fluid>
+                {/* <div className='flex justify-center items-center h-screen'>
+                    <Spin size='large' />
+                </div>
 
-            <div className='p-4'>
-                <Alert message="Error" description={"error"} type='error' showIcon />
-            </div>
+                <div className='p-4'>
+                    <Alert message="Error" description={"error"} type='error' showIcon />
+                </div>
 
-            <div className='p-4'>
-                <Alert message="Assignment not found" description="The requested assignment could not be found." type='warning' showIcon />
-            </div>
+                <div className='p-4'>
+                    <Alert message="Assignment not found" description="The requested assignment could not be found." type='warning' showIcon />
+                </div> */}
 
+                <div>
+                    <header className='bg-teal-600 text-white p-4 rounded-lg mb-4'>
+                        <h1 className='text-2xl font-semibold flex items-center gap-3'>
+                            <button className='border-2 p-2 text-xl rounded-full hover:border-sky-blue transition duration-200'>
+                                <FaArrowLeft />
+                            </button>
+                            Make a Restaurant Landing Page
+                        </h1>
+                        <p className='text-md ml-14'>Make sure the landing page is responsive and good looking</p>
+                    </header>
+
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-2'>
+                        <section className='col-span-2'>
+                            <div className=' rounded-lg p-4'>
+                                <div className='mb-4'>
+                                    <h2 className='text-3xl flex gap-3 items-center'>
+                                        <LiaClipboardListSolid /> Make a Restaurant Landing Page
+                                    </h2>
+                                    <h3 className='ms-11 mt-2 text-gray-700'>
+                                        Due: {new Date().toLocaleDateString()}
+                                    </h3>
+                                </div>
+                                <div className='bg-gray-100 p-4 rounded-lg shadow mb-4 mt-3'>
+                                    <p>Make sure the landing page is responsive and good looking</p>
+                                    <div className='border-t-2 mt-4 pt-4'>
+                                        <h2 className='text-xl font-bold mb-3'>Assignment File:</h2>
+                                        <Button
+                                            type='primary'
+                                            icon={<FaDownload />}
+                                            href='#'
+                                            target='_blank'
+                                        >
+                                            Download Assignment
+                                        </Button>
+                                        <p>No file attached to this assignment.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section className='bg-gray-100 p-4 rounded-lg shadow h-max'>
+
+                            <h2 className='text-xl mb-4'>Your Submission</h2>
+
+                            <div className='mb-4'>
+                                <p>Submitted on: {new Date().toLocaleString()}</p>
+                                <p>Status <Tag color='green'>
+                                    Evaluated
+                                </Tag></p>
+                                <p>Total Marks: 10</p>
+
+                                <p>Obtained Marks: 5</p>
+                                <Progress
+                                    percent={Math.round((56.5) * 100)}
+                                    status='active'
+                                />
+                            </div>
+
+                            <div className='mt-4'>
+                                <h3 className='font-bold mb-2'>Rating:</h3>
+                                <p>5</p>
+                            </div>
+
+                            <div className='mt-4'>
+                                <h3 className='font-bold mb-2'>Remark:</h3>
+                                <p>Good</p>
+                            </div>
+
+                            <div className='mt-3'>
+                                <Button className='w-full text-sky-blue'>Unsubmit</Button>
+                            </div>
+
+                            <div>
+                                <Dropdown
+                                    menu={{
+                                        items,
+                                        onClick
+                                    }}
+                                    trigger={['click']}
+                                >
+                                    <Button className='w-full text-sky-blue'>
+                                        <Space>
+                                            <FaPlus /> Add or create
+                                        </Space>
+                                    </Button>
+                                </Dropdown>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+
+
+                <Modal
+                    title="Submit Assignment"
+                    open={submitModalVisible}
+                    onCancel={handleSubmitCancel}
+                    footer={null}
+                >
+                    <>
+                        <Upload
+                            beforeUpload={(file) => {
+                                return false;
+                            }}
+                        >
+                            <Button icon={<FaPlus />}>
+                                Uploading
+                            </Button>
+
+                        </Upload>
+
+                        <Progress percent={70} status='active' />
+
+                        <Button
+                            type='primary'
+                            style={{ marginTop: 16 }}
+                        >
+                            Submit
+                        </Button>
+                    </>
+
+                    <>
+                        <Input
+                            type='text'
+                            placeholder='Enter your submission here...'
+                        />
+                        <Button
+                            type='primary'
+                            style={{ marginTop: 16 }}
+                        >
+                            Submit
+                        </Button>
+                    </>
+
+                </Modal>
+
+            </Container>
         </div>
     )
 }
